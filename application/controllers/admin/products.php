e45bbd48b80882a9347655890770d853774771ab
Vladislav Mosnak <vmosnak@yahoo.com>
2016-03-13 18:30:20 +0100

    autoload url hepler, remove footer and header from products/add.php

--- application/controllers/admin/products.php
+++ application/controllers/admin/products.php
@@ -5,11 +5,7 @@ class Products extends CI_Controller {
 
     public function index()
     {
-<<<<<<< HEAD:application/controllers/admin/shoes.php
-        $this->load->view('admin/shoes.php');
-=======
         $this->load->view('admin/products/list.php');
->>>>>>> 0116594653c719a48ef3bddbfd76af8f9ea39ef7:application/controllers/admin/products.php
     }
 
     public function edit($id)
